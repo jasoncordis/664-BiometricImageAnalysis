fbac0d4c3d1ff499477f47d47af4ccf6bb4afda7
jasoncordis <cordisjason@gmail.com>
2022-03-10 15:02:03 -0800

    commit

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from scipy import ndimage as ndi
 from skimage.util import random_noise
 from skimage import feature
 
-
 image = io.imread('/664/664-BiometricImageAnalysis/images/frames_grey/0001.png')
 
 #Threshold
@@ -15,16 +14,13 @@ thresh = threshold_mean(image)
 binary = image > thresh
 
 fig, axes = plt.subplots(ncols=2, figsize=(8, 3))
-ax = axes.ravel()
+output = axes.ravel()
 
-ax[0].imshow(image, cmap=plt.cm.gray)
-ax[0].set_title('Original image')
+output[0].imshow(image, cmap=plt.cm.gray)
+output[0].set_title('Original image')
 
-ax[1].imshow(binary, cmap=plt.cm.gray)
-ax[1].set_title('Result')
-
-for a in ax:
-    a.axis('off')
+output[1].imshow(binary, cmap=plt.cm.gray)
+output[1].set_title('Result')
 
 plt.show()
 
